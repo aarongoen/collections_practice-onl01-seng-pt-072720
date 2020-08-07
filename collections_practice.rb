53782581ab01bce073a2d82c03d42ae2cf8a4768
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-07 21:39:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,6 +3,13 @@ def sort_array_asc(integers)
 end 
 
 def sort_array_desc(integers)
-  integers.sort {|a, b| 
+  integers.sort do |a, b| 
+    if a == b 
+      0 
+    elsif a < b 
+      1 
+      elsif a > b
+      -1
+    end 
 end 
 
