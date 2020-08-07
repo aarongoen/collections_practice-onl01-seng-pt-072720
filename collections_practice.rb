437619240d2397838d6791b7a4ac28ecb903ac60
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-07 21:55:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,14 +15,15 @@ def sort_array_desc(integers)
 end 
 
 def sort_array_char_count(strings)
-  strings.sort.length do |a, b|
-     if a.length == b.length
-      0 
-    elsif a.length < b.length
-      -1 
-    elsif a.length > b.length
-      1
-    end 
+  strings.sort do |a, b|
+     a.length <=> b.length
+    # if a.length == b.length
+    #   0 
+    # elsif a.length < b.length
+    #   -1 
+    # elsif a.length > b.length
+    #   1
+    # end 
   end 
 end 
 
