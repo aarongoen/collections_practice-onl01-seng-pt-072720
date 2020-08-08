d3a409d289ea6dad56542e24d568a5fd7e004d86
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-08 01:25:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,9 +49,9 @@ def kesha_maker(strings)
   return new_array
 end 
 
-# def find_a(array)
-#   array.select { |string| string.start_with?("a") }
-# end 
+def find_a(array)
+  array.select { |string| string.start_with?("a") }
+end 
  
 def sum_array(integers)
 sum = 0
